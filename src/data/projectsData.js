41915d98f982cf1a9e17d0406bd018d2aca1cd88
Afaq Hussain <109997730+Aafak18@users.noreply.github.com>
2023-01-02 12:55:47 +0500

    Update projectsData.js

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,13 +1,6 @@
-import one from '../assets/svg/projects/one.svg'
-import two from '../assets/svg/projects/two.svg'
-import three from '../assets/svg/projects/three.svg'
-import four from '../assets/svg/projects/four.svg'
-import five from '../assets/svg/projects/five.svg'
-import six from '../assets/svg/projects/six.svg'
-import seven from '../assets/svg/projects/seven.svg'
-import eight from '../assets/svg/projects/eight.svg'
-import budget from '../assets/png/budget.png'
-import expense from '../assets/png/expense.png'
+import budget from '../assets/png/budget.png';
+import expense from '../assets/png/expense.png';
+import portfoliox from '../assets/png/portfoliox.png';
 
 
 export const projectsData = [
@@ -29,48 +22,149 @@ export const projectsData = [
         demo: 'budgetplannerbyafaq.surge.sh',
         image: budget
     },
-    // {
-    //     id: 3,
-    //     projectName: 'Educative Clone',
-    //     projectDesc: 'This project is the clone of Educative.io. Built as part of Project week, my contributions include the home page(both with user and without user), course information page and checkout page. The rest of the pages and features were built by other members of my team',
-    //     tags: ['React', 'Material UI', 'Express', "MongoDB","NodeJS"],
-    //     code: 'https://github.com/sreerag-rajan/educative-clone',
-    //     demo: 'https://educative-clone.vercel.app/',
-    //     image: "https://user-images.githubusercontent.com/85482640/161571651-35ab8e4e-dc57-4834-bafc-9485766f5b8a.png"
-    // },
-    // {
-    //     id: 4,
-    //     projectName: 'Reliance Digital Clone - Backend Integration',
-    //     projectDesc: 'This is the clone of reliancedigital.in. Features I worked on the Product Catalogue pages and also on some minor parts on other pages as well',
-    //     tags: ['EJS', 'Express', 'MongoDB', 'Node.js'],
-    //     code: 'https://github.com/sreerag-rajan/Reliance-Digital-Clone-U4-',
-    //     demo: 'https://reliance-digital-clone.herokuapp.com/',
-    //     image: "https://miro.medium.com/max/875/1*qRNfRbuHCDC1ct0chLZoWA.png"
-    // },
-    // {
-    //     id: 5,
-    //     projectName: 'Reliance Digital Clone - Frontend',
-    //     projectDesc: 'This project is the clone of Reliance Digital built as a part of a project week. It simulates the functionality of an e-commerce website from landing page to checkout. My contribution was the product catalog page arriving from the navbar, as well as leading the team.',
-    //     tags: ['HTML', 'CSS', 'Javascript'],
-    //     code: 'https://github.com/iamrituyadav/Reliance_digital',
-    //     demo: 'https://reliance-digital-clone.herokuapp.com/',
-    //     image: "https://miro.medium.com/max/875/1*qRNfRbuHCDC1ct0chLZoWA.png"
-    // },
+    {
+        id: 3,
+        projectName: 'Portfolio',
+        projectDesc: 'This project is the clone of Educative.io. Built as part of Project week, my contributions include the home page(both with user and without user), course information page and checkout page. The rest of the pages and features were built by other members of my team',
+        tags: ['HTML' ,'CSS', 'Javascript', 'React'],
+        //code: 'https://github.com/sreerag-rajan/educative-clone',
+        demo: 'https://afaqsportfoliox.netlify.app/',
+        image: portfoliox
+    },
+    {
+        id: 4,
+        projectName: 'Extract text from PDF in Python',
+        projectDesc: ' Used Python to create this project. Installed the PyPDF2 Python library specifically for this task. By entering the pip command at your terminal or command prompt, this library can be quickly installed. When dealing with resumes, this ability is helpful.',
+        tags: ['Python'],
+        code: 'https://github.com/Aafak18/Extract-Text-From-PDF',
+        //demo: 'https://githuacb.com/Aafak18/Extract-Text-From-PDF',
+        image:''
+    },
+    {
+        id: 5,
+        projectName: 'Dice-Roll-Simulator-with-Python',
+        projectDesc: 'll be utilising Pythons random module to simulate a dice roll. The random module comes preinstalled in the Python programming language, making it simple to import it into your code.',
+        tags: ['Python'],
+        code: 'https://github.com/Aafak18/Dice-Roll-Simulator-with-Python',
+        //demo: 'https://afaqsportfoliox.netlify.app/',
+        image:''
+    },
+    {
+        id: 6,
+        projectName: 'Password-Generator-by-python',
+        projectDesc: 'This Python program generates password for user by taking input of number of characters of password from user.',
+        tags: ['Pyhton'],
+        code: 'https://github.com/Aafak18/Password-Generator-by-python',
+        //demo: 'budgetplannerbyafaq.surge.sh',
+        image: ''
+    },
+    {
+        id: 7,
+        projectName: 'Digital-clock-in-C',
+        projectDesc: 'It takes input in hours, mins & seconds and displays the clock running forward from that time.',
+        tags: ['C/C++'],
+        code: 'https://github.com/Aafak18/Digital-clock-in-C',
+        //demo: 'https://afaqsportfoliox.netlify.app/',
+        image:''
+    },
+    {
+        id: 8,
+        projectName: 'Student-Management-System',
+        projectDesc: 'Developed a straightforward C++ program called Student Management System.Created it for educational institutions to manage student data and various other student-related data requirements. In this Student Management System project, the user can construct, display, and modify. Implemented C/C++ as the technology used to create the project. Used the C++ programming language to create the database, and the data is not permanently saved. From 1 to 500, we can add as many different students as we like. Set up the software in a way that we must enter their name, roll number, class, subject, and contact details. We can add students, view student information by roll number, edit student information, or delete a specific students information',
+        tags: ['C/C++'],
+        code: 'https://github.com/Aafak18/Student-Management-System',
+        //demo: 'https://afaqsportfoliox.netlify.app/',
+        image: ''
+    },
+    {
+        id: 9,
+        projectName: 'Snake-Game-C/C++',
+        projectDesc: 'The snake game is made in C/C++.',
+        tags: ['C/C++'],
+        code: 'https://github.com/Aafak18/Snakegamecpp',
+        //demo: 'https://afaqsportfoliox.netlify.app/',
+        image: ''
+    },
+    {
+        id: 10,
+        projectName: 'ToDo List by TypeScript',
+        projectDesc: 'The project I made is made using TypeScript, HTML, CSS & JavaScript. It simply takes the input as daily errands from a user and adds them to the list. This is purely made by TypeScript. This app shows real-time addition of new errands and tasks which are pending and needs to be done. ',
+        tags: ['Html', 'CSS', 'JavaScript', 'TypeScript'],
+        demo: 'https://todolistbyafaq.netlify.app/',
+        //code: 'https://afaqsportfoliox.netlify.app/',
+        image: ''
+    },
+    {
+        id: 11,
+        projectName: 'Weather App in React',
+        projectDesc: '• Built this project using React, Redux, CSS, HTML, and JavaScript. Set up the software in a way that takes input as a city name and returns the weather information of that city in the run time. The data (API) of weather is fetched by the website:https://openweathermap.org/current.',
+        tags: ['Html', 'CSS', 'JavaScript', 'React'],
+        demo: 'https://weatherappbyafaq.netlify.app/',
+        //code: 'https://afaqsportfoliox.netlify.app/',
+        image: ''
+    },
+    {
+        id: 12,
+        projectName: 'Covid Tracker with Graph',
+        projectDesc: 'Developed a Covid Tracker to stay up-to-date with valuable coronavirus (COVID-19) data summarized by confirmed cases, geography, testing and treatment, projections, and economic impact.Made the project using React, Redux, CSS, HTML, and JavaScript. Set up the software in a way that shows the active cases, recovered, and death count of the people. Enabled it to show data of worldwide people infected as well as country-wise. ',
+        tags: ['Html', 'CSS', 'JavaScript', 'React'],
+        //code: 'https://covidtrackerbyafaq.netlify.app/',
+        demo: 'https://covidtrackerbyafaq.netlify.app/',
+        image: ''
+    },
+    {
+        id: 13,
+        projectName: 'Expense Tracker in TypeScript',
+        projectDesc: 'The project I made is made using TypeScript, HTML, CSS & JavaScript. It simply takes the input as expenses and calculates the total expenses and Cash amount left after all data calculations. This is purely made by TypeScript. This app shows real-time amount changes as expenses are added or income is added. It is much easier on that. ',
+        tags: ['Html', 'CSS', 'JavaScript', 'TypeScript'],
+        code: 'https://github.com/Aafak18/Expense-Tracker-by-Typescript',
+        //demo: 'https://github.com/Aafak18',
+        image: ''
+    },
+    {
+        id: 14,
+        projectName: 'Currency Converter in Java',
+        projectDesc: 'Developed a Java programming mini-project currency converter. It offers a web-based interface for converting money between different currencies.Set up the software in a way that the currency, amount, and currency to be converted are all inputs. This project also converts the amount and currency entered into a number of other currencies that are supported.',
+        tags: ['Java'],
+        code: 'https://github.com/Aafak18',
+        //demo: 'https://afaqsportfoliox.netlify.app/',
+        image: ''
+    },
+    {
+        id: 15,
+        projectName: 'Billing System in Java',
+        projectDesc: ' Developed a system for managing invoices, customers, payments, transactions, and login details. Set up the software in a way that it oversees the management of all billing, transaction history, login, and billing information. The project was developed entirely for admins, so only admins are allowed access. In addition to the primary component, the project contains 5 Java files: login, maincontroller, order, table, and user. This project receives orders from the user window, after which the administrator can view the order, the table, and the user name.The order is then delivered after that.',
+        tags: ['Java'],
+        code: 'https://github.com/Aafak18',
+        //demo: 'https://afaqsportfoliox.netlify.app/',
+        image: ''
+    },
+    {
+        id: 16,
+        projectName: 'Two factor Authenticator',
+        projectDesc: 'Our 2-factor authentication app is a security tool that adds an extra layer of protection to your online accounts. It works by requiring you to enter a one-time code, in addition to your password, whenever you log in. This code is generated by the app and sent to your phone via SMS or push notification. Our app is built using Node.js and Express.',
+        tags: ['HTML', 'CSS', 'JavaScript', 'NodeJS', 'ExpressJS'],
+        code: 'https://github.com/Aafak18/2-Factor-Authentication',
+        //demo: 'https://afaqsportfoliox.netlify.app/',
+        image: ''
+    },
+    {
+        id: 15,
+        projectName: 'Searching-downloading-analyzing-automation-python',
+        projectDesc: 'This python program runs to the given url, search for the required file in html, downloads the file, place the file in desired folder and then makes the data analyzing report according to the requirement There are different libraries and modules used in this program to search on google create virtual Google chrome controlled by automation, numpy and pandas.',
+        tags: ['Python'],
+        code: 'https://github.com/Aafak18/searching-downloading-analyzing-automation-python',
+        //demo: 'https://afaqsportfoliox.netlify.app/',
+        image: ''
+    },
+    {
+        id: 16,
+        projectName: 'Jarvis(Your Voice Assistance) in Python',
+        projectDesc: 'An AI program that can tell the time, set an alarm and perform internet searches when commands are given. This program also has a calculator, can play, pause, increase or decrease the volume of a video, can schedule the day and upon request tells us the schedule. It can send an email on our behalf without using a browser and can open programs like Word and Excel.',
+        tags: ['Python'],
+        code: 'https://github.com/Aafak18/Jarvis-in-Python',
+        //demo: 'https://afaqsportfoliox.netlify.app/',
+        image: ''
+    },
     
 ]
-
-
-// Do not remove any fields.
-// Leave it blank instead as shown below
-
-/* 
-{
-    id: 1,
-    projectName: 'Car Pooling System',
-    projectDesc: '',
-    tags: ['Flutter', 'React'],
-    code: '',
-    demo: '',
-    image: ''
-}, 
-*/
