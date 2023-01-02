d3a6303e0a6fe0ff6b4f91cc23418480cbcde78e
Afaq Hussain <109997730+Aafak18@users.noreply.github.com>
2023-01-02 12:54:47 +0500

    Update achievementData.js

--- src/data/achievementData.js
+++ src/data/achievementData.js
@@ -1,10 +1,14 @@
 import html from '../assets/png/html.jpeg'
 import css from '..//assets/png/css.jpeg'
 import react from '../assets/png/react.jpeg'
-// import python from '../assets/png/python.jpeg'
+import python0 from '../assets/png/python0.png'
 import ads from '../assets/png/ads.jpeg'
 import analytics from '../assets/png/analytics.jpeg'
-
+import elements from '../assets/png/elements.jpeg'
+import studio from '../assets/png/studio.jpeg'
+import business from '../assets/png/business.jpeg'
+import c from '../assets/png/c.jpeg'
+import Python1 from '../assets/png/Python1.jpeg'
 
 export const achievementData = {
     bio : "The illiterate of the 21st century will not be those who cannot read and write, but those who cannot learn, unlearn, and relearn. - Alvin Toffler",
@@ -34,16 +38,16 @@ export const achievementData = {
             image : react
         },
         {
-            id : 3,
+            id : 4,
             title : 'Programming in Python certified',
             details : 'Python Programming Language Course',
             date : 'Sept 8, 2022',
             field : 'Back-end Developement',
-            image : css
+            image : python0
         },
 
         {
-            id : 4,
+            id : 5,
             title : 'HTML 5 Application Developement Certificate',
             details : 'HTML5 Certifaction for Application Developement',
             date : 'Sept 7, 2022',
@@ -51,7 +55,7 @@ export const achievementData = {
             image : html
         },
         {
-            id : 5,
+            id : 6,
             title : 'Programming in HTML5 with JavaScript & CSS3',
             details : 'Certifiaction for front-end developement',
             date : 'Sept 7, 2022',
@@ -59,7 +63,47 @@ export const achievementData = {
             image : css
         },
         {
-            id : 6,
+            id : 7,
+            title : 'Programming with C/C++',
+            details : 'Certification for Development in C/C++',
+            date : 'Oct 3, 2022',
+            field : 'Programming with C/C++',
+            image : c
+        },
+        {
+            id : 8,
+            title : 'Elements of AI',
+            details : 'Certification for Development in AI',
+            date : 'November 17, 2022',
+            field : 'Programming with AI',
+            image : elements
+        },
+        {
+            id : 9,
+            title : 'Google My Business',
+            details : 'A certification for Google My Business',
+            date : 'Sept 25, 2022',
+            field : 'Google My Business',
+            image : business
+        },
+        {
+            id : 10,
+            title : 'Google Studio',
+            details : 'A certification for Google Studio ',
+            date : 'Sept 25, 2022',
+            field : 'Google Studio',
+            image : studio
+        },
+        {
+            id : 11,
+            title : 'Programming with Python 3.X',
+            details : 'Certification of Programming with Python 3.X',
+            date : 'Dec 12, 2022',
+            field : 'Programming with Python 3.X',
+            image : Python1
+        },
+        {
+            id : 12,
             title : 'Speed Programming in C/C++',
             details : 'Speed programming certification in C/C++',
             date : 'Feb 1, 2016',
